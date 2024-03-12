c954c3760723e57ca854154886325979e549663f
arieshug <163071937+arieshug@users.noreply.github.com>
2024-03-12 11:17:56 +0900

    GiftBox 추가
    GiftBox에서 Letter와 Money를 넣고 Friend도 넣어줬다. Public도 써보았다

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace ConsoleApp1
 {
+    class GiftBox
+    {
+        public string Letter;
+        public int Money;
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GiftBox a = new GiftBox()
+            {
+                Letter = "잘 지내지?",
+                Money = 100000
+            };
+            GiftBox Friend = a;
+
+            a.Letter = "어떻게 지내고 있어?";
+            a.Money = 150000;
         }
     }
 }

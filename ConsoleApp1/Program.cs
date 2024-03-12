47ceb0d6c93f4a1907f4e9c716ccafc2f546e9d9
arieshug <163071937+arieshug@users.noreply.github.com>
2024-03-12 11:57:53 +0900

    Texting
    A to friend, friend to A
    text log

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -14,10 +14,25 @@
                 Letter = "잘 지내지?",
                 Money = 100000
             };
+            Console.WriteLine("A 문자내용");
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
             GiftBox Friend = a;
+
+            Console.WriteLine("친구 문자내용");
+            Console.WriteLine(Friend.Letter);
+            Console.WriteLine(Friend.Money);
 
             a.Letter = "어떻게 지내고 있어?";
             a.Money = 150000;
+
+            Console.WriteLine("A 변경 이후 문자내용");
+            Console.WriteLine(a.Letter);
+            Console.WriteLine(a.Money);
+
+            Console.WriteLine("친구 문자 내용");
+            Console.WriteLine(Friend.Letter);
+            Console.WriteLine(Friend.Money);
         }
     }
 }
